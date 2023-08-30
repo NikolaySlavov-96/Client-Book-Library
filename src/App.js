fe120991b066d696027719015c9726bdd12ea07a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-30 15:06:28 +0300

    created and initialization client application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is web page for track your book reading history and exchange of experience</h1>
+    </div>
+  );
+}
+
+export default App;
