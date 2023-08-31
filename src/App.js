ca06be16f05ef650b29eb22f3b800f1d8af5e2f0
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-31 17:26:43 +0300

    using base components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import { AuthProvide } from './contexts/AuthContext';
+import { Home } from './component/Home/Home';
+import { Book } from './component/Book/Book';
+import { BookProvider } from './contexts/BookContext';
+
+
 function App() {
-  return (
-    <div className="App">
-      <h1>This is web page for track your book reading history and exchange of experience</h1>
-    </div>
-  );
+    return (
+        <AuthProvide>
+            <BookProvider>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/book' element={<Book />} />
+                </Routes>
+            </BookProvider>
+        </AuthProvide>
+    );
 }
 
 export default App;
