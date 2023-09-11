010ccc61b1bcb840f25390a0be72f96111721702
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-11 20:46:58 +0300

    creating pagination logic and a little style

--- src/component/Book/Pagenation/Pagenation.js
+++ src/component/Book/Pagenation/Pagenation.js
@@ -1,9 +1,17 @@
+import { useBookContext } from "../../../contexts/BookContext";
+
+import style from './Pagenation.module.css';
+
 export const Pagenation = ({ books }) => {
 
-    const page = Math.ceil(books / 10) || 0;
+    const { setPage, page, limit } = useBookContext({});
+    const count = Math.ceil(books / limit) || 0;
+
     return (
-        <div>
+        <div className={style['pagination__container']}>
+            <button onClick={() => (page - 1) && setPage(page - 1)}>-</button>
             <p>{page}</p>
+            <button onClick={() => (count >= (page + 1)) ? setPage(page + 1) : ''}>+</button>
         </div>
     );
 }
