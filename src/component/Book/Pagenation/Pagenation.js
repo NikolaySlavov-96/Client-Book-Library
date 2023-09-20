3911344909e15f1d57d10cf4180e0dcef0f7519b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-20 21:37:01 +0300

    creating pagination interactive and adding style

--- src/component/Book/Pagenation/Pagenation.js
+++ src/component/Book/Pagenation/Pagenation.js
@@ -9,9 +9,29 @@ export const Pagenation = ({ books }) => {
 
     return (
         <div className={style['pagination__container']}>
-            <button onClick={() => (page - 1) && setPage(page - 1)}>-</button>
-            <p>{page}</p>
-            <button onClick={() => (count >= (page + 1)) ? setPage(page + 1) : ''}>+</button>
+            <button className={style["page"]} onClick={() => (page - 1) > 0 && setPage(page - 1)}>&#x3c;</button>
+            {
+                page - 2 > 0 && (
+                    <button className={style["page"]} onClick={() => setPage(page - 2)}>{page - 2}</button>
+                )
+            }
+            {
+                page - 1 > 0 && (
+                    <button className={style["page"]} onClick={() => setPage(page - 1)}>{page - 1}</button>
+                )
+            }
+            <p className={`${style["page__current"]} ${style["page"]}`} >{page}</p>
+            {
+                count >= page + 1 && (
+                    <button className={style["page"]} onClick={() => setPage(page + 1)}>{page + 1}</button>
+                )
+            }
+            {
+                count >= page + 2 && (
+                    <button className={style["page"]} onClick={() => setPage(page + 2)}>{page + 2}</button>
+                )
+            }
+            <button className={style["page"]} onClick={() => (count >= (page + 1)) ? setPage(page + 1) : ''}>&#x3e;</button>
         </div>
     );
 }
