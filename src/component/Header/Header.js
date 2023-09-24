fd0be15a784ea934552669b928fec87011302a90
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-24 15:40:31 +0300

    enhanced and styling header / navigation

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
-
 import { useAuthContext } from "../../contexts/AuthContext";
+
+import style from './Header.module.css';
 
 
 export const Header = () => {
@@ -9,22 +10,29 @@ export const Header = () => {
     const name = email?.split('@')[0];
 
     return (
-        <div>
-            <h1>Hello {name ? name : 'Gues'}</h1>
-
-            <nav>
-                <ul>
+        <header className={style['navigation__container']}>
+            <nav className={style['navigation__nav']}>
+                <h1>Hello {name ? name : 'Gues'}</h1>
+                <ul className={style['navigation__ul']}>
                     <li><NavLink to='/book'>Books</NavLink></li>
+                    {
+                        !email && (<>
+                            <li><NavLink to='/auth/login'>Login</NavLink></li>
+                            <li><NavLink to='/auth/register'>Register</NavLink></li>
+                        </>)
+                    }
+
                     {
                         email && (<>
                             <li><NavLink to='/userbook/forreading'>For Reading</NavLink></li>
                             <li><NavLink to='/userbook/reading'>Reading</NavLink></li>
                             <li><NavLink to='/userbook/forpurchase'>For Purchase</NavLink></li>
                             <li><NavLink to='/userbook/purchase'>Purchase</NavLink></li>
+                            <li><NavLink to='/auth/logout'>Logout</NavLink></li>
                         </>)
                     }
                 </ul>
             </nav>
-        </div>
+        </header>
     );
 }
