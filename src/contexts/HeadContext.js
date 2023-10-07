c00df6191d4c795db212506288195207dfc9fcd2
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-10-07 12:38:46 +0300

    creating context for change title and meta data

---
+++ src/contexts/HeadContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+
+const HeadContext = createContext();
+
+export const HeadProvider = ({ children }) => {
+    const [title, setTitle] = useState('Home');
+
+    const contextValue = {
+        title,
+        setTitle,
+    }
+
+    return (
+        <HeadContext.Provider value={contextValue} >
+            {children}
+        </HeadContext.Provider >
+    )
+}
+
+
+export const useHeadContext = () => {
+    const context = useContext(HeadContext);
+    return context;
+}
