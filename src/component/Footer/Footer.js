41105e4fa7b810e236de76ec0ad49c2d56fbedf7
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-02-08 23:07:58 +0200

    up footer style

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
-import './Footer.module.css';
+import style from './Footer.module.css';
 
 export const Footer = () => {
     return (
         <footer className='global__bg-radius'>
-            <div className="shadow">
+            <div className={`'shadow' ${style['div']}`}>
                 <p>Designed and Implement from <Link to='https://slavo-v.dev'>Slavo-v</Link></p>
             </div>
         </footer>
