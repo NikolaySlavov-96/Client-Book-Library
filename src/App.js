e2c741e92e26338d8b00197a80fdd2f29a3c3cf0
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-07 16:16:46 +0300

    feat: Attach SocketProvider to App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Header, Helmet, Footer } from './component/UI';
 import { VerifyAccount, Register, Login, Books, CreateBook, DetailsForBook } from './component/Screens';
 
 import ROUT_NAMES from './Constants/routNames';
+import { SocketProvider } from './contexts/SocketContext';
 
 
 const App = () => {
@@ -19,16 +20,18 @@ const App = () => {
                 <Header />
 
                 <BookProvider>
-                    <Routes>
-                        <Route path={ROUT_NAMES.HOME} element={<Books />} />
-                        <Route path={ROUT_NAMES.BOOK} element={<Books />} />
-                        <Route path={ROUT_NAMES.CREATE_BOOK} element={<CreateBook />} />
-                        <Route path={ROUT_NAMES.BOOK_DETAILS} element={<DetailsForBook />} />
-                        <Route path={ROUT_NAMES.USER_COLLECTION} element={<Books />} />
-                        <Route path={ROUT_NAMES.LOGIN} element={<Login />} />
-                        <Route path={ROUT_NAMES.REGISTER} element={<Register />} />
-                        <Route path={ROUT_NAMES.VERIFY_TOKEN} element={<VerifyAccount />} />
-                    </Routes>
+                    <SocketProvider>
+                        <Routes>
+                            <Route path={ROUT_NAMES.HOME} element={<Books />} />
+                            <Route path={ROUT_NAMES.BOOK} element={<Books />} />
+                            <Route path={ROUT_NAMES.CREATE_BOOK} element={<CreateBook />} />
+                            <Route path={ROUT_NAMES.BOOK_DETAILS} element={<DetailsForBook />} />
+                            <Route path={ROUT_NAMES.USER_COLLECTION} element={<Books />} />
+                            <Route path={ROUT_NAMES.LOGIN} element={<Login />} />
+                            <Route path={ROUT_NAMES.REGISTER} element={<Register />} />
+                            <Route path={ROUT_NAMES.VERIFY_TOKEN} element={<VerifyAccount />} />
+                        </Routes>
+                    </SocketProvider>
                 </BookProvider>
             </AuthProvide>
             <Footer />
