aaf66c3b5c5b280115bb07473f8d279368461615
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-08 16:25:46 +0300

    refactor: Extract button titles into constants for better maintainability

--- src/component/UI/Header/_Header.js
+++ src/component/UI/Header/_Header.js
@@ -1,37 +1,50 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { NavLink } from "react-router-dom";
 
 import { useAuthContext } from "../../../contexts/AuthContext";
 
 import style from './Header.module.css';
+import ROUT_NAMES from "../../../Constants/routNames";
 
+const FIRST_NAME = 'Guest';
+const HEADER_BUTTON_TITLES = {
+    BOOK: 'Book',
+    CREATE_BOOK: 'Create',
+    LOGIN: 'Login',
+    REGISTER: 'Register',
+    COLLECTION_OF_BOOKS: 'Collections of Books',
+    LOGOUT: 'Logout',
+}
 
 const _Header = () => {
-
     const { email, onSubmitLogout } = useAuthContext();
     const name = email?.split('@')[0];
+
+    const onPressLogout = useCallback(() => onSubmitLogout(), [onSubmitLogout]);
 
     return (
         <header className={`global__bg-radius ${style['navigation__container']}`}>
             <nav className={style['navigation__nav']}>
-                <h1>Hello {name ? name : 'Guest'}</h1>
+                <h1>Hello {name ? name : FIRST_NAME}</h1>
+
                 <ul className={style['navigation__ul']}>
-                    <li><NavLink to='/book'>Books</NavLink></li>
+                    <li><NavLink to={ROUT_NAMES.BOOK}>{HEADER_BUTTON_TITLES.BOOK}</NavLink></li>
                     {
                         !email && (<>
-                            <li><NavLink to='/auth/login'>Login</NavLink></li>
-                            <li><NavLink to='/auth/register'>Register</NavLink></li>
+                            <li><NavLink to={ROUT_NAMES.LOGIN}>{HEADER_BUTTON_TITLES.LOGIN}</NavLink></li>
+                            <li><NavLink to={ROUT_NAMES.REGISTER}>{HEADER_BUTTON_TITLES.REGISTER}</NavLink></li>
                         </>)
                     }
 
                     {
                         email && (<>
-                            <li><NavLink to='/create'>Create</NavLink></li>
-                            <li><NavLink to='/collections'>Collections Of Books</NavLink></li>
-                            <li><a onClick={() => onSubmitLogout()}>Logout</a></li>
+                            <li><NavLink to={ROUT_NAMES.CREATE_BOOK}>{HEADER_BUTTON_TITLES.CREATE_BOOK}</NavLink></li>
+                            <li><NavLink to={ROUT_NAMES.USER_COLLECTION}>{HEADER_BUTTON_TITLES.COLLECTION_OF_BOOKS}</NavLink></li>
+                            <li><a onClick={onPressLogout}>{HEADER_BUTTON_TITLES.LOGOUT}</a></li>
                         </>)
                     }
                 </ul>
+
             </nav>
         </header>
     );
