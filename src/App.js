e76d595a6217af5ed6e1dcbe3bd5ecb819c56022
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-13 19:28:15 +0300

    refactor: Sort and organize App file for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 
 import { AuthProvide } from './contexts/AuthContext';
 import { BookProvider } from './contexts/BookContext';
+import { SocketProvider } from './contexts/SocketContext';
 
 import { Header, Helmet, Footer } from './component/UI';
 import { VerifyAccount, Register, Login, Books, CreateBook, DetailsForBook, SearchByEmail, UserCollection } from './component/Screens';
 import ModalContainer from './component/Modals';
 
 import { ROUT_NAMES } from './Constants';
-import { SocketProvider } from './contexts/SocketContext';
-
 
 const App = () => {
     return (
