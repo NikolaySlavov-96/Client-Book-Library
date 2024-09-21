977dc7afc46c91da695c6c402e1fcf676f95ef1a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-21 14:39:11 +0300

    feat: Create custom Link component based on react-router-dom Link

--- src/component/atoms/index.js
+++ src/component/atoms/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/_Button';
 export { default as InputField } from './InputField/_InputField';
+export { default as Link } from './Link/_Link';
 export { default as NavigationButton } from './NavigationButton/_NavigationButton';
 export { default as SectionTitle } from './SectionTitle/_SectionTitle';
