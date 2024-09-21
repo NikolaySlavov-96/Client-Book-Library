975e6f18f446cedd2d59e1f7187dba0467e705b5
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-21 14:06:04 +0300

    feat: Create SectionTitle component with H1 element

--- src/component/atoms/index.js
+++ src/component/atoms/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/_Button';
 export { default as InputField } from './InputField/_InputField';
 export { default as NavigationButton } from './NavigationButton/_NavigationButton';
+export { default as SectionTitle } from './SectionTitle/_SectionTitle';
