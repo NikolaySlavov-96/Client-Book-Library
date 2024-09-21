47c6e11cc99832c5cb6928862cae9cdd80aca337
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-21 18:08:40 +0300

    feat: Create ActionButton component with icon

--- src/component/atoms/index.js
+++ src/component/atoms/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/_Button';
+export { default as IconActionButton } from './IconActionButton/_IconActionButton';
 export { default as InputField } from './InputField/_InputField';
 export { default as Link } from './Link/_Link';
 export { default as NavigationButton } from './NavigationButton/_NavigationButton';
