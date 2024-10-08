67bfc20df28cc2ed20bfdbab94f44cbce3e49d46
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-08 19:40:41 +0300

    test: Add new test events

--- src/Constants/_socketEvents.ts
+++ src/Constants/_socketEvents.ts
@@ -1,8 +1,15 @@
 export enum _EReceiveEvents {
     NEW_BOOK_ADDED = 'new-book-added',
     USER_JOINED = 'user_joined',
+    SUPPORT_CHAT_USER_JOIN_ACKNOWLEDGMENT = 'support_chat_user_join_acknowledgment',
+    SEND_SUPPORT_MESSAGE = 'send_support_message',
+    NOTIFY_ADMINS_OF_NEW_USER = 'notify_admins_of_new_user',
+    SUPPORT_ACCEPT_USER_ACKNOWLEDGMENT = 'support_accept_user_acknowledgment',
 };
 
 export enum _ESendEvents {
     USER_JOINED = 'user_joined',
+    SUPPORT_CHAT_USER_JOIN = 'support_chat_join',
+    SUPPORT_ACCEPT_USER = 'support_accept_user',
+    RECEIVE_SUPPORT_MESSAGE = 'receive_support_message',
 };
--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -5,6 +5,7 @@ import { SocketService } from '../services';
 import { EReceiveEvents, ESendEvents, MODAL_NAMES } from '../Constants';
 
 import { useGetUserAddress, useStoreZ } from "../hooks";
+import { useAuthContext } from "./AuthContext";
 
 interface ISocketContext { }
 
@@ -12,6 +13,7 @@ const SocketContext = createContext<ISocketContext | undefined>(undefined);
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
 
+    const { email } = useAuthContext();
     const { openModal, setModalName, setContent, } = useStoreZ();
 
     const userAddressData = useGetUserAddress();
@@ -22,24 +24,48 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
         openModal();
     }, [setModalName, setContent, openModal]);
 
+    const updateCountOfVisitors = (data: any) => {
+        console.log(data)
+    }
+
+    const saveConnectId = (data: { connectId: string }) => {
+        // Save connectId in localStorage
+        console.log("🚀 ~ saveConnectId ~ data:", data)
+    }
+
+    const notifyAdmin = (data: any) => console.log('NOTIFY_ADMINS_OF_NEW_USER', data);
+
+
     useEffect(() => {
         SocketService.connect();
 
         SocketService.subscribeToEvent(EReceiveEvents.NEW_BOOK_ADDED, result);
-        SocketService.subscribeToEvent(EReceiveEvents.USER_JOINED, (data) => console.log(data));
+        SocketService.subscribeToEvent(EReceiveEvents.USER_JOINED, updateCountOfVisitors);
+        SocketService.subscribeToEvent(EReceiveEvents.SUPPORT_CHAT_USER_JOIN_ACKNOWLEDGMENT, saveConnectId);
+
+        SocketService.subscribeToEvent(EReceiveEvents.NOTIFY_ADMINS_OF_NEW_USER, notifyAdmin);
 
         return () => {
-            SocketService.unsubscribeFromEvent(EReceiveEvents.NEW_BOOK_ADDED, () => console.log(';222'))
+            SocketService.unsubscribeFromEvent(EReceiveEvents.NEW_BOOK_ADDED, () => console.log('Unsubscribe NEW_BOOK_ADDED'))
             SocketService.disconnect();
         }
     }, []);
 
     useEffect(() => {
-        console.log("🚀 ~ useEffect ~ userAddressData:", userAddressData)
         if (userAddressData.hasOwnProperty('IPv4')) {
             SocketService.sendData(ESendEvents.USER_JOINED, userAddressData);
         }
     }, [userAddressData]);
+
+    // Moved in Chat window
+    // Run this at press open chatWindow
+    useEffect(() => {
+        if (email) {
+            // SocketService.sendData(ESendEvents.SUPPORT_CHAT_USER_JOIN, { connectId: 'da51f92c-35b9-4c7a-9705-da4aa5f58102', });
+        } else {
+            // SocketService.sendOnlySignal(ESendEvents.SUPPORT_CHAT_USER_JOIN);
+        }
+    }, [email]);
 
     return (
         <SocketContext.Provider value={SocketService}>
