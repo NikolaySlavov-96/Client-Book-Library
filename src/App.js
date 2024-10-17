928af69668eddedc3ee13fc6c55f51af73e1add3
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-17 19:48:47 +0300

    refactor: Replace existing navigation with extracted navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
-
 import './App.css';
 
 import { AuthProvide } from './contexts/AuthContext';
@@ -7,21 +5,9 @@ import { BookProvider } from './contexts/BookContext';
 import { SocketProvider } from './contexts/SocketContext';
 
 import { Footer, Header } from './component/molecules';
-import { CustomerSupportChat, ModalContainer, UserQueueForSupportModule } from './component/organisms';
-import {
-    Books,
-    CreateBook,
-    DetailsForBook,
-    Helmet,
-    Login,
-    NotFound,
-    Register,
-    SearchByEmail,
-    UserCollection,
-    VerifyAccount,
-} from './component/Screens';
+import { Helmet } from './component/Screens';
 
-import { ROUT_NAMES } from './Constants';
+import { Navigator } from './Utils';
 
 
 const App = () => {
@@ -32,22 +18,7 @@ const App = () => {
                 <Header />
                 <BookProvider>
                     <SocketProvider>
-                        <Routes>
-                            <Route path={ROUT_NAMES.HOME} element={<Books />} />
-                            <Route path={ROUT_NAMES.BOOK} element={<Books />} />
-                            {/* Route Defense */}
-                            <Route path={ROUT_NAMES.REVIEW_BOOKS_BY_EMAIL} element={<SearchByEmail />} />
-                            <Route path={ROUT_NAMES.CREATE_BOOK} element={<CreateBook />} />
-                            <Route path={ROUT_NAMES.BOOK_DETAILS} element={<DetailsForBook />} />
-                            <Route path={ROUT_NAMES.USER_COLLECTION} element={<UserCollection />} />
-                            <Route path={ROUT_NAMES.LOGIN} element={<Login />} />
-                            <Route path={ROUT_NAMES.REGISTER} element={<Register />} />
-                            <Route path={ROUT_NAMES.VERIFY_TOKEN} element={<VerifyAccount />} />
-                            <Route path='*' element={<NotFound />} />
-                        </Routes>
-                        <ModalContainer />
-                        <CustomerSupportChat />
-                        <UserQueueForSupportModule />
+                        <Navigator />
                     </SocketProvider>
                 </BookProvider>
             </AuthProvide>
