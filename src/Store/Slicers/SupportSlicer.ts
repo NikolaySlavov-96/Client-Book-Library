9f9c1d5d0b0762c8ea5f1754eac629e8c1673000
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-29 19:25:36 +0200

    feat: Enhance SupportSlicer to accept welcome message and manage room/messages

--- src/Store/Slicers/SupportSlicer.ts
+++ src/Store/Slicers/SupportSlicer.ts
@@ -10,27 +10,74 @@ export interface IUserQueue {
     status: "waiting"
 }
 
-export interface IRoom {
-    roomName: string;
+interface IMessage {
     message: string;
 }
 
+interface IMessages {
+    [key: string]: IMessage[];
+}
+
+export interface IRoom {
+    roomName: string;
+}
+
 export interface SupportSlicer {
+    welcomeMessage: string;
+    setWelcomeMessage: (message: string) => void;
     users: IUserQueue[];
     setUsers: (newData: IUserQueue[]) => void;
     rooms: IRoom[];
-    setRooms: (newRoom: IRoom)  => void;
+    setRooms: (newRoom: IRoom) => void;
+    updateRoom: (roomName: string, newData: IRoom) => void;
+    messages: IMessages;
+    addMessage: (roomName: string, data: IMessage) => void;
+    updateMessageState: (roomName: string, data: IMessage) => void;
 }
 
 const createSupportSlicer: StateCreator<SupportSlicer> = (set) => ({
+    welcomeMessage: '',
+    setWelcomeMessage: (message: string) => set(state => ({
+        welcomeMessage: message,
+    })),
+
     users: [],
     setUsers: (newUser) => set(state => ({
         users: newUser,
     })),
+
     rooms: [],
     setRooms: (newRoom) => set(state => ({
         rooms: [...state.rooms, newRoom],
     })),
+    updateRoom: (roomName, newData) => set(state => {
+        return {
+            rooms: state.rooms.map(r => r.roomName === roomName ? newData : r)
+        }
+    }),
+
+    messages: {},
+    addMessage: (roomName, data) => set((state) => {
+        const currentMessages = state.messages[roomName] || [];
+        return {
+            messages: {
+                ...state.messages,
+                [roomName]: [...currentMessages, data],
+            },
+        };
+    }),
+    updateMessageState: (roomName, newMessageState) => set((state) => {
+        const currentMessage = state.messages[roomName] || {};
+        return {
+            messages: {
+                ...state.messages,
+                [roomName]: {
+                    ...currentMessage,
+                    ...newMessageState,
+                },
+            },
+        };
+    }),
 });
 
 export default createSupportSlicer;
