99489beaf264823ddebfecfaf9a31d0c41777be3
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-31 14:37:08 +0200

    refactor: Enhance useLocalStorage for specific use case

--- src/hooks/_useLocalStorage.ts
+++ src/hooks/_useLocalStorage.ts
@@ -1,7 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import { useStoreZ } from ".";
+
+import { STORAGE_KEYS } from "../Constants";
 
 
 const _useLocalStorage = (key: string, initialValue: {}) => {
+    const { setConnectId } = useStoreZ();
+
     const [state, setState] = useState(() => {
         const persist = localStorage.getItem(key);
 
@@ -11,6 +17,21 @@ const _useLocalStorage = (key: string, initialValue: {}) => {
         }
         return initialValue;
     });
+
+    useEffect(() => {
+        if (key === STORAGE_KEYS.CONNECT_ID) {
+            const persist = localStorage.getItem(key);
+            if (persist) {
+                const connectId = JSON.parse(persist);
+                setConnectId(connectId)
+            }
+        }
+    }, []);
+
+    // const saveToLocalStorage = (value) => {
+    //     setData(value);
+    //     localStorage.setItem(key, JSON.stringify(value));
+    //   };
 
     const setLocalStorage = (value: any) => {
         setState(value);
