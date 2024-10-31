f2e81199a57d5cb4122b6002ef49b3041f39ee90
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-31 14:35:45 +0200

    feat: Implement and attach Common slicer to store

---
+++ src/Store/Slicers/CommonSlicer.ts
@@ -0,0 +1,13 @@
+import { StateCreator } from "zustand";
+
+export interface ICommonSlicer {
+    connectId: string,
+    setConnectId: (id: string) => void;
+}
+
+const createCommonSlicer: StateCreator<ICommonSlicer> = (set) => ({
+    connectId: '',
+    setConnectId: (id) => set({ connectId: id })
+});
+
+export default createCommonSlicer;
--- src/hooks/_useStoreZ.ts
+++ src/hooks/_useStoreZ.ts
@@ -1,13 +1,21 @@
 import { create } from "zustand";
+import { persist } from 'zustand/middleware';
 
+import createCommonSlicer, { ICommonSlicer } from "../Store/Slicers/CommonSlicer";
 import createModalSlicer, { ModalSlicer } from "../Store/Slicers/ModalSlicer";
 import createSupportSlicer, { SupportSlicer } from "../Store/Slicers/SupportSlicer";
 
-type StoreState = ModalSlicer & SupportSlicer;
+import { STORAGE_KEYS } from "../Constants";
+
+// const commonSlicerPersist = persist(createCommonSlicer, {
+//     name: STORAGE_KEYS.CONNECT_ID
+// })
+type StoreState = ModalSlicer & SupportSlicer & ICommonSlicer;
 
 const _useStoreZ = create<StoreState>((set, get, store) => ({
     ...createModalSlicer(set, get, store),
     ...createSupportSlicer(set, get, store),
+    ...createCommonSlicer(set, get, store),
 }));
 
 export default _useStoreZ;
