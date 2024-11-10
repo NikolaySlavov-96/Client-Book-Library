38c25f8eec4cb7dba77a76693560ad6d4d5cec47
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-10 11:01:14 +0200

    feat: Attach product slicer to useStoreZ hook

--- src/hooks/_useStoreZ.ts
+++ src/hooks/_useStoreZ.ts
@@ -4,18 +4,20 @@ import { persist } from 'zustand/middleware';
 import createCommonSlicer, { ICommonSlicer } from "../Store/Slicers/CommonSlicer";
 import createModalSlicer, { IModalSlicer } from "../Store/Slicers/ModalSlicer";
 import createSupportSlicer, { ISupportSlicer } from "../Store/Slicers/SupportSlicer";
+import createProductSlicer, { IProductSlicer } from "../Store/Slicers/ProductSlicer";
 
 import { STORAGE_KEYS } from "../Constants";
 
 // const commonSlicerPersist = persist(createCommonSlicer, {
 //     name: STORAGE_KEYS.CONNECT_ID
 // })
-type StoreState = IModalSlicer & ISupportSlicer & ICommonSlicer;
+type StoreState = IModalSlicer & ISupportSlicer & ICommonSlicer & IProductSlicer;
 
 const _useStoreZ = create<StoreState>((set, get, store) => ({
     ...createModalSlicer(set, get, store),
     ...createSupportSlicer(set, get, store),
     ...createCommonSlicer(set, get, store),
+    ...createProductSlicer(set, get, store),
 }));
 
 export default _useStoreZ;
