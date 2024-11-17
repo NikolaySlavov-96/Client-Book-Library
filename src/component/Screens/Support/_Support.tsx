1dc13105bc790f72150107605f14b6b221ac21be
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-17 10:57:55 +0200

    refactor: Replace map with List component in Support screen and Select component

--- src/component/Screens/Support/_Support.tsx
+++ src/component/Screens/Support/_Support.tsx
@@ -1,6 +1,6 @@
 import { memo, useCallback, useEffect } from 'react'
 
-import { ChatHeader } from '../../../component/atoms';
+import { ChatHeader, List } from '../../../component/atoms';
 
 import { useStoreZ } from '../../../hooks';
 
@@ -13,13 +13,57 @@ import style from './_Support.module.css';
 
 const DEFAULT_TITLE = 'Support Chat - ';
 
+const keyExtractorUser = (item: any) => item.connectId.toString();
+const keyExtractorRoom = (item: any) => item.roomName.toString();
+const keyExtractorMessage = (item: any) => item.roomName.toString();
+
+
 const _Support = () => {
     const { rooms, connectId, users, messages, selectedRoom, setSelectedRoom } = useStoreZ();
     const { email } = useAuthContext();
 
-    const onAcceptUser = useCallback((userConnectId: string) => {
-        SocketService.sendData(ESendEvents.SUPPORT_ACCEPT_USER, { supportId: connectId, acceptUserId: userConnectId });
-    }, [connectId])
+    const currentRoomMessages = messages[selectedRoom] || [];
+
+    const renderItemUser = useCallback(({ item, }: { item: any }) => {
+        const onClick = () => {
+            SocketService.sendData(ESendEvents.SUPPORT_ACCEPT_USER, { supportId: connectId, acceptUserId: item.connectId });
+        };
+
+        return (
+            <button
+                onClick={onClick}>
+                {item.connectId}
+            </button>
+        )
+    }, [connectId]);
+
+    const renderItemRoom = useCallback(({ item, }: { item: any }) => {
+        const onClick = () => {
+            setSelectedRoom(item.roomName)
+        };
+
+        return (
+            <button
+                onClick={onClick}
+                style={{ display: 'inline-block', marginRight: 10, }}>
+                {item.roomName.slice(0, 5)}
+            </button>
+        )
+    }, [setSelectedRoom]);
+
+    const renderItemMessage = useCallback(({ item, }: { item: any }) => {
+        const onClick = () => {
+            setSelectedRoom(item.roomName)
+        };
+
+        return (
+            <button
+                onClick={onClick}
+                style={{ display: 'inline-block', marginRight: 10, }}>
+                {item.roomName.slice(0, 5)}
+            </button>
+        )
+    }, [setSelectedRoom]);
 
     useEffect(() => {
         SocketService.sendData(ESendEvents.SUPPORT_CHAT_USER_JOIN, { connectId, });
@@ -31,37 +75,27 @@ const _Support = () => {
                 <ChatHeader>
                     <p>{`${DEFAULT_TITLE}${email.split('@')[0]}`}</p>
                 </ChatHeader>
-                <div className={style['list-chat__container']}>
-                    <>
-                        {users.map(u => {
-                            return (
-                                <button
-                                    key={u.connectId}
-                                    onClick={() => onAcceptUser(u.connectId)}>
-                                    {u.connectId}
-                                </button>
-                            )
-                        })}
-                    </>
-                </div>
+                <List
+                    data={users}
+                    renderItem={renderItemUser}
+                    keyExtractor={keyExtractorUser}
+                    style={style['list-chat__container']}
+                />
             </div>
             <div className={style['room__container']}>
-                <div className={style['room__header']}>
-                    {
-                        rooms.map(r =>
-                            <button
-                                onClick={() => setSelectedRoom(r.roomName)}
-                                style={{ display: 'inline-block', marginRight: 10, }}
-                                key={r.roomName}>
-                                {r.roomName.slice(0, 5)}</button>
-                        )
-                    }
-                </div>
-                <div className={style['chat__window']}>
-                    <>
-                        {selectedRoom !== '' ? messages[selectedRoom].map(m => <p key={m.message}>{m.message}</p>) : null}
-                    </>
-                </div>
+                <List
+                    data={rooms}
+                    renderItem={renderItemRoom}
+                    keyExtractor={keyExtractorRoom}
+                    style={style['room__header']}
+                />
+                <List
+                    data={currentRoomMessages}
+                    renderItem={renderItemMessage}
+                    keyExtractor={keyExtractorMessage}
+                    EmptyComponent={() => null}
+                    style={style['chat__window']}
+                />
             </div>
         </section>
     )
