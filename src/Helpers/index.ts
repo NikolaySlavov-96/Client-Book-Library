3fc50bb9056ee88374701a03728542a7b85111d0
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-12-03 20:22:35 +0200

    feat: Implement simple date formatter

--- src/Helpers/index.ts
+++ src/Helpers/index.ts
@@ -2,3 +2,5 @@ export { default as API } from './_API';
 export { default as FormatSelectOptions } from './_FormatSelectOptions';
 export { default as SocketHelper } from './_SocketHelper';
 export { default as TitleFromLocation } from './_TitleFromLocation';
+
+export { convertDateTime } from './_Date';
