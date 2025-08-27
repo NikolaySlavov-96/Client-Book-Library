17b43214239b3f9bd7f6faa814a50f51e112bd0d
Nikolay Slavov <nikolay.slavov.96@gmail.com>
2025-08-27 14:37:40 +0300

    Typo error

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export { _EReceiveEvents as EReceiveEvents } from './_socketEvents';
 export { _ESendEvents as ESendEvents } from './_socketEvents';
-export { default as MODAL_NAMES } from './_molalNames';
+export { default as MODAL_NAMES } from './_modalNames';
 export { default as QUERY_LIMIT } from './_queryLimit';
 export { default as ROUT_NAMES } from './_routNames';
 export { default as SEARCH_NAME } from './_searchName';
